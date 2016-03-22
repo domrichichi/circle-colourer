096906c4faa354ee370f73ada92d5dbbf7a830cf
domrichichi <domrichichi@gmail.com>
2016-03-22 12:42:52 -0400

    added variables

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 var $html = $('html');
-var $ball = $('.ball');
+var $ball = $('div');
 var $picker = $('form');
-var $input = $('#colour-picker');
-
-$picker.on('change', function () {
-  $ball.css('background-color', $input.val());
-});
+var $input = $('#pick-color');
